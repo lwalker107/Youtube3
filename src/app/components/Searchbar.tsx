ad4cb06c8246bb5581329c43491d8eb5f5283b90
lwalker <landen@example.com>
2024-10-13 10:31:57 -0500

    magnifying glasses icon is now a working button inside the search field

--- src/app/components/Searchbar.tsx
+++ src/app/components/Searchbar.tsx
@@ -38,7 +38,7 @@ export default function SearchBar() {
             e.preventDefault();
             handleSearch((e.target as HTMLFormElement).search.value)
         }}
-        className='flex'>
+        className='relative flex w-full max-w-md'>
             <input
                 id='search'
                 name='search'
@@ -46,11 +46,13 @@ export default function SearchBar() {
                 // Input value is tied to the state
                 defaultValue={searchParams.get('query')?.toString()}
                 placeholder="Search.."
-                className='p-4 rounded-full border-4'
+                className='w-full p-3 pl-10 rounded-full border-2 border-gray-300 focus:border-green-500 outline-none'
             />
 
-            <button type='submit'>
-                <MagnifyingGlassIcon className='size-10 text-green-700' />
+            <button 
+                type="submit"
+                className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-green-500">
+                    <MagnifyingGlassIcon className="h-5 w-5" />
             </button>
         </form>
     );
